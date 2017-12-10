4464ca4488ee4afcfb86dedec99b3566ee321b84
raabit <731615400@qq.com>
2017-12-10 23:24:22 +0800

    增加星星评级

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
+/**
+ * 样式列表
+ */
 var iconList = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-anchor', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb', 'fa-leaf', 'fa-bomb', 'fa-bolt', 'fa-bicycle', 'fa-paper-plane-o', 'fa-cube'];
 /*
  * Display the cards on the page
@@ -23,7 +26,10 @@ function shuffle(array) {
 
     return array;
 }
-
+/**
+ * 初始化所有卡片样式
+ * @param {Array} array 
+ */
 function render(array) {
     var cards = document.getElementsByClassName('card');
     array.forEach(function(icon, index) {
@@ -41,27 +47,30 @@ function render(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-var cardOpen;
-var count = 0;
-var moves = 0;
-
+var cardOpen; // 点击单个元素
+var count = 0; // 统计匹配成功的数量 8个即为胜利
+var moves = 0; // 计算步数
+var timersId = []; // 存放timeoutId列表
+/**
+ * 页面游戏元素点击
+ */
 document.getElementsByClassName('deck')[0].onclick = function(me) {
     meSrc = me.srcElement;
     if (meSrc.className.indexOf('card') === -1 || meSrc.className.indexOf('card match') > -1 || meSrc === cardOpen) return;
     meSrc.className = 'card open show';
     if (!cardOpen) return cardOpen = meSrc;
     moves++;
-
+    // 匹配成功
     if (cardOpen.children[0].className === meSrc.children[0].className) {
         count++;
         meSrc.className = cardOpen.className += 'card match animated rubberBand';
         setTimeout(function() {
-            meSrc.className = cardOpen.className = 'card match';
-            cardOpen = undefined;
-        }, 300)
-        if (count === 8) {
-            show(document.getElementById('winbox'));
-        }
+                meSrc.className = cardOpen.className = 'card match';
+                cardOpen = undefined;
+            }, 300)
+            // 游戏胜利
+        if (count === 8) show(document.getElementById('winbox'));
+        // 匹配失败
     } else {
         meSrc.className = cardOpen.className += 'card open show animated shake';
         currCardOpen = cardOpen;
@@ -70,33 +79,71 @@ document.getElementsByClassName('deck')[0].onclick = function(me) {
         }, 300)
         cardOpen = null;
     }
+    // 更新页面上的步数
     document.getElementsByClassName('moves')[0].innerText = moves;
     document.getElementsByClassName('moves')[1].innerText = moves;
 };
-
+/**
+ * 初始化游戏
+ */
 function reset() {
+    updateStar(3);
+    timersId.forEach(function(timerId) {
+        clearTimeout(timerId);
+    })
+    timersId = [];
     cardOpen = null;
     count = 0;
     moves = 0;
+    setTimeUpdateStar([2 * 60 * 1000, 5 * 60 * 1000, 10 * 60 * 1000]);
     document.getElementsByClassName('moves')[0].innerText = 0;
     render(shuffle(iconList))
 }
-
+/**
+ * 隐藏元素
+ */
 function hide(ele) {
     ele.style.display = "none";
 }
-
+/**
+ * 显示元素
+ */
 function show(ele) {
     ele.style.display = "block";
 }
-
-function getStar(rate) {
-    return '★★★☆☆☆'.slice(3 - rate, 6 - rate);
+/**
+ * 更新页面星星数
+ * @param {Number} number
+ */
+function updateStar(number) {
+    var stars = Array.from(document.getElementsByClassName('stars')[0].children);
+    document.getElementsByClassName('stars')[1].innerText = number;
+    stars.forEach(function(star, index) {
+        star.children[0].className = index < number ? 'fa fa-star' : 'fa fa-star-o';
+    })
+}
+/**
+ * 设置星星评级时间规则
+ * @param {Array} timers 例如[2*60*1000, 5*60*1000, 10*60*1000];
+ */
+function setTimeUpdateStar(timers) {
+    timersId.push(setTimeout(function() {
+        updateStar(2);
+        timersId.push(setTimeout(function() {
+            updateStar(1);
+            timersId.push(setTimeout(function() {
+                updateStar(0);
+            }, timers[0]))
+        }, timers[1]))
+    }, timers[0]))
 }
 
+// 游戏过程中重置游戏
 document.getElementsByClassName('restart')[0].onclick = reset;
+// 胜利时点击重置游戏
 document.getElementsByClassName('restart')[1].onclick = function() {
     hide(document.getElementById('winbox'));
     reset();
 };
-document.addEventListener('DOMContentLoaded', reset)
+// 首次加载初始化游戏
+document.addEventListener('DOMContentLoaded', reset);
